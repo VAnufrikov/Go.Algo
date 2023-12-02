8d04198b49d13c3b02aaa3570d247a8d93fc5813
Valentin Anufrikov <v.anufrikov@yandex.ru>
2023-12-02 17:26:13 +0300

    add file ranking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 from upload_data.upload import read_data_stock, upload_data_from_moexalgo
+from upload_data.Ranking import ranking
 
 
 SRCH_MOEX = 'data/ListingSecurityList.csv'
@@ -10,8 +11,14 @@ DATE_END = date(2023, 10, 10)
 if __name__ == '__main__':
     listing = read_data_stock(SRCH_MOEX)
 
+    # TODO сделать алгоритм ранжирования по всем акциям согласно критериям анализа акций
+    # и записать это в функцию ranking() #
+    # выходом этой функции будет DF список тикетов для загрузки за даты moexalgo #
+    ranking(DATE_START, DATE_END)
+
     # Получили пока что 1 TRADE_CODE по которому мы будем учится
     df = upload_data_from_moexalgo(listing['TRADE_CODE'][0], DATE_START, DATE_END)
+
 
     print(df.head())
 
