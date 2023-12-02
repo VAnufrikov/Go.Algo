91badcbd984231b37634dfba9489d1ba10f08356
Valentin Anufrikov <v.anufrikov@yandex.ru>
2023-12-02 17:20:32 +0300

    add upload in new file

--- main.py
+++ main.py
@@ -1,61 +1,17 @@
-from datetime import date, timedelta
+from datetime import date
+from upload_data.upload import read_data_stock, upload_data_from_moexalgo
 
-import pandas as pd
-from moexalgo import Market, Ticker
-import time
 
+SRCH_MOEX = 'data/ListingSecurityList.csv'
 LIMIT = 10000
 DATE_START = date(2023, 10, 1)
 DATE_END = date(2023, 10, 10)
 
-
-def read_data_stock(srch):
-    """Подготавливаем датасет со всеми компаниями ан московской бирже"""
-    df = pd.read_csv('data/ListingSecurityList.csv', engine='python', encoding='cp1251', sep=';')
-
-    df = df[['TRADE_CODE', 'EMITENT_FULL_NAME', 'INSTRUMENT_TYPE', 'LIST_SECTION', 'INSTRUMENT_CATEGORY', 'CURRENCY',
-             'NOMINAL', 'ISSUE_AMOUNT']]
-
-    # Берем только акции#
-    df = df[(df['INSTRUMENT_TYPE'] == 'Акция обыкновенная') | (df['INSTRUMENT_TYPE'] == 'Акции иностранного эмитента')]
-
-    return df.reset_index(drop=True)
-
-def perdelta(start, end):
-    curr = start
-    while curr < end:
-        yield curr
-        curr += timedelta(days=1)
-
-def get_dates():
-    list_dates = []
-
-    for result in perdelta(DATE_START, DATE_END):
-        list_dates.append(result)
-
-    list_dates.append(DATE_END)
-
-    return list_dates
-
-
-def upload_data_from_moexalgo(TRADE_CODE):
-    """Получаем данные по TRADE_CODE из moexalgo"""
-    dates = get_dates()
-
-    tradestats = pd.DataFrame()
-    for date in dates:
-        url = f'https://iss.moex.com/iss/datashop/algopack/eq/tradestats/{TRADE_CODE}.csv?from={date}&till={date}&iss.only=data'
-        df = pd.read_csv(url, sep=';', skiprows=1)
-        tradestats = pd.concat([tradestats, df])
-        time.sleep(0.5)
-
-    return tradestats #tradestats.to_csv(f'tradestats_{TRADE_CODE}.csv', index=None)
-
-
-
 if __name__ == '__main__':
-    listing = read_data_stock('data/ListingSecurityList.csv')
+    listing = read_data_stock(SRCH_MOEX)
 
-    df = upload_data_from_moexalgo(listing['TRADE_CODE'][0])
+    # Получили пока что 1 TRADE_CODE по которому мы будем учится
+    df = upload_data_from_moexalgo(listing['TRADE_CODE'][0], DATE_START, DATE_END)
+
     print(df.head())
 
---
+++ upload_data/upload.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+import pandas as pd
+import time
+
+
+
+def read_data_stock(srch):
+    """Подготавливаем датасет со всеми компаниями ан московской бирже"""
+    df = pd.read_csv('data/ListingSecurityList.csv', engine='python', encoding='cp1251', sep=';')
+
+    df = df[['TRADE_CODE', 'EMITENT_FULL_NAME', 'INSTRUMENT_TYPE', 'LIST_SECTION', 'INSTRUMENT_CATEGORY', 'CURRENCY',
+             'NOMINAL', 'ISSUE_AMOUNT']]
+
+    # Берем только акции#
+    df = df[(df['INSTRUMENT_TYPE'] == 'Акция обыкновенная') | (df['INSTRUMENT_TYPE'] == 'Акции иностранного эмитента')]
+
+    return df.reset_index(drop=True)
+
+def perdelta(start, end):
+    curr = start
+    while curr < end:
+        yield curr
+        curr += timedelta(days=1)
+
+def get_dates(DATE_START, DATE_END):
+    list_dates = []
+
+    for result in perdelta(DATE_START, DATE_END):
+        list_dates.append(result)
+
+    list_dates.append(DATE_END)
+
+    return list_dates
+
+
+def upload_data_from_moexalgo(TRADE_CODE, DATE_START, DATE_END):
+    """Получаем данные по TRADE_CODE из moexalgo"""
+    dates = get_dates(DATE_START, DATE_END)
+
+    tradestats = pd.DataFrame()
+    for date in dates:
+        url = f'https://iss.moex.com/iss/datashop/algopack/eq/tradestats/{TRADE_CODE}.csv?from={date}&till={date}&iss.only=data'
+        df = pd.read_csv(url, sep=';', skiprows=1)
+        tradestats = pd.concat([tradestats, df])
+        time.sleep(0.5)
+    return tradestats
