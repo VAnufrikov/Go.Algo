39f016d04c55eb748e05bf18b50e73852031d603
Завен Мартиросян <z.martirosian@vk.team>
2023-12-08 18:52:32 +0300

    add functionality for purchase stocks evenly

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 from datetime import date
 
-SRCH_MOEX = 'data/ListingSecurityList.csv'
-LIMIT = 10000
-DATE_START = date(2023, 10, 1)
-DATE_END = date(2023, 10, 10)
+class Config:
+    STOCKS_PATH = "stock_portfolio.csv"
+    SRCH_MOEX = 'data/ListingSecurityList.csv'
+    LIMIT = 10000
+    DATE_START = date(2023, 10, 1)
+    DATE_END = date(2023, 10, 10)
--- stock_portfolio/agent.py
+++ stock_portfolio/agent.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import random
 from etna.datasets.tsdataset import TSDataset
 from stock_portfolio.portfolio import Stocks
-from settings import DATE_START, DATE_END, LIMIT
+from settings import Config
 import etna
 
 
@@ -13,7 +14,7 @@ def run_agent():
 
     tiket = portfel.get_tiket()
 
-    df_between_dates = portfel.get_time_baket(tiket, DATE_START, DATE_END)
+    df_between_dates = portfel.get_time_baket(tiket, Config.DATE_START, Config.DATE_END)
 
     inside = agent.predict(df_between_dates)
 
@@ -49,7 +50,7 @@ class Agent:
 
     def __int__(self):
         """Инициализация агента и его портфеля"""
-        self.limit = LIMIT
+        self.limit = Config.LIMIT
 
     def predict(self, df):
         df.loc[:, 'trade_datetime'] = pd.to_datetime(df.tradedate.astype(str) + ' ' + df.tradetime.astype(str))
@@ -69,9 +70,11 @@ class Agent:
          """
         return 0
 
-    def by(self):
+    def by(self, ticket, count, price):
         """Реализация выставление тикета в стакан на покупку"""
-        pass
+        str_line = f"{ticket}|{price}|{count}\n"
+        with open(Config.STOCKS_PATH, 'a') as fout:
+            fout.write(str_line)
 
     def sell(self):
         """Реализация выставление тикета в стакан на продажу"""
@@ -80,3 +83,52 @@ class Agent:
     def do_nofing(self):
         """Не выставляем тикет и просто ждем, возвращаем действие ничего не делаем"""
         pass
+
+    def get_prices(self, stocks: list) -> list[tuple]:
+        """ Получить закупочную стоимость акций, которые необходимо купить
+        Args:
+            stocks: список акций, которые необходимо купить.
+        Returns:
+            prices_list: [("ticket", "price")] - список цен акций
+        """
+        prices_list = [(ticket, self.get_ticket_price(ticket)) for ticket in stocks]
+        return prices_list
+
+    def get_ticket_price(self, ticket: str) -> int:
+        """ Получить закупочную стоимость конкретной акции
+        Args:
+            ticket: название акции
+        Returns:
+            price: цена акции
+        """
+        #TODO: изменить на получение реальной цены
+        price = random.randint(100, 1000)
+        return price
+
+    def count_stocks_values(self, prices_list: list, limit: int) -> list[tuple]:
+        """ Посчитать соотношение акций к покупке
+        Args:
+            prices_list: список акций с ценами
+            limit: максимальная сумма стоимости акций
+        Returns:
+            stocks_count: [("ticket", "count")] - количество акций к покупке
+        """
+        max_price_for_one_bucket = limit/len(prices_list)
+        stocks_count = [(ticket_info[0], max_price_for_one_bucket//ticket_info[1])
+                         for ticket_info in prices_list]
+        return stocks_count
+
+    def fill_stock_portfolio(self, stocks: list, limit: int) -> None:
+        """ Заполнить портфель равномерно акциями на максимальную сумму
+        Args:
+            stocks: названия акций для покупки
+            limit: максимальная сумма стоимости акций
+        Returns:
+            None
+        """
+        prices_list = self.get_prices(stocks)
+        stocks_count = self.count_stocks_values(prices_list, limit)
+        for stock_info in stocks_count:
+            ticket_name = stock_info[0]
+            ticket_count = stock_info[1]
+            self.by(ticket=ticket_name, count=ticket_count, price=stock_info[1])
