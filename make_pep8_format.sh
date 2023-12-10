75d0b878f31a04744f789f39f05a63c99eb9d322
Valentin Anufrikov <v.anufrikov@yandex.ru>
2023-12-10 09:43:39 +0300

    add make_pep8_format.sh

---
+++ make_pep8_format.sh
@@ -0,0 +1,17 @@
+python3 -m pip install black
+python3 -m pip install isort
+python3 -m black checker/checker.py
+python3 -m black news_regressor/*.py
+python3 -m black sqlite/*.py
+python3 -m black stock_portfolio/*.py
+python3 -m black upload_data/*.py
+python3 -m black settings.py
+python3 -m black main.py
+
+python3 -m isort checker/checker.py
+python3 -m isort news_regressor/*.py
+python3 -m isort sqlite/*.py
+python3 -m isort stock_portfolio/*.py
+python3 -m isort upload_data/*.py
+python3 -m isort settings.py
+python3 -m isort main.py
